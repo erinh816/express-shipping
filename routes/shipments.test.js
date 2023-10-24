c6d4b3d756f02f1884c820a78bc7ac5ed0ead9eb
Silu Hu <erinh816@gmail.com>
2023-10-24 19:32:59 -0400

    add post mock

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,9 +3,10 @@
 const request = require("supertest");
 const fetchMock = require("fetch-mock");
 
-const app = require("../app");
-const { shipProduct } = require("../shipItApi");
+let { shipProduct } = require("../shipItApi");
 shipProduct = jest.fn();
+
+const app = require("../app");
 
 const { json } = require("express");
 
@@ -22,9 +23,29 @@ describe("POST /", function () {
 
     expect(shipId).toEqual({ shipped: 7544 });
   });
+  test('valid post requeset', async function () {
+    fetchMock.post(`/shipments`, {
+      body: {
+        shipped: 7544
+      },
+      status: 200
+    });
+
+    const res = await shipProduct({
+      body: {
+        productId: 1000,
+        name: "Test Tester",
+        addr: "100 Test St",
+        zip: "12345-6789"
+      }
+    });
+    expect(res).toEqual({ shipped: 7544 });
+  });
 
   test("throws error if empty request body", async function () {
     const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
   });
 });
+
+//TODO:test for part1
