6926b4744a0a956cbd63614a14140031012e3bdf
Logan Winnie <loganwinnier@gmail.com>
2023-10-24 19:13:23 -0400

    started test post cr

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,25 +1,30 @@
 "use strict";
 
 const request = require("supertest");
-const app = require("../app");
+const fetchMock = require("fetch-mock");
 
+const app = require("../app");
+const { shipProduct } = require("../shipItApi");
+shipProduct = jest.fn();
+
+const { json } = require("express");
 
 describe("POST /", function () {
   test("valid", async function () {
-    const resp = await request(app).post("/shipments").send({
+    shipProduct.mockReturnValue({ shipped: 7544 });
+
+    const shipId = await shipProduct({
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(shipId).toEqual({ shipped: 7544 });
   });
 
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
     expect(resp.statusCode).toEqual(400);
   });
 });
