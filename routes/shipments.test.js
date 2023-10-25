7aecd9d8d6e5e8ebc67ea0aaed91a39c7ff4c849
Silu Hu <erinh816@gmail.com>
2023-10-24 21:14:35 -0400

    get tests done

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,69 +1,52 @@
 "use strict";
 
 const request = require("supertest");
-const fetchMock = require("fetch-mock");
 
-let { shipProduct } = require("../shipItApi");
-shipProduct = jest.fn();
+let shipItAPI = require("../shipItApi");
+shipItAPI.shipProduct = jest.fn();
 
 const app = require("../app");
 
 describe("POST / tests", function () {
-  shipProduct.mockReturnValue({ shipped: 7544 });
 
-  // test("ship product", async function () {
-  //   const shipId = await shipProduct({
-  //     productId: 1000,
-  //     name: "Test Tester",
-  //     addr: "100 Test St",
-  //     zip: "12345-6789",
-  //   });
 
-  //   expect(shipId).toEqual({ shipped: 7544 });
-  // });
+  test("ship product", async function () {
+    shipItAPI.shipProduct.mockReturnValue(7544);
 
-  test("valid post request", async function () {
-    fetchMock.post(`/shipments`, {
-      body: {
-        shipped: 7544,
-      },
-      status: 200,
-    });
-
-    const res = await shipProduct({
-      body: {
-        productId: 1000,
-        name: "Test Tester",
-        addr: "100 Test St",
-        zip: "12345-6789",
-      },
-    });
-
-    expect(res).toEqual({ shipped: 7544 });
-  });
-
-  test("throws error if empty request body", async function () {
-    const resp = await request(app).post("/shipments").send();
-    expect(resp.statusCode).toEqual(400);
-  });
-
-  test("throws error if invalid request body", async function () {
     const resp = await request(app).post("/shipments").send({
-      productId: "ds",
+      productId: 1000,
       name: "okay",
-      addr: true,
+      addr: '100 test street',
       zip: "2139019",
     });
 
-    expect(resp.statusCode).toEqual(400);
-    expect(resp.body).toEqual({
-      error: {
-        message: [
-          "instance.productId is not of a type(s) integer",
-          "instance.addr is not of a type(s) string",
-        ],
-        status: 400,
-      },
-    });
+    expect(resp.body).toEqual({ shipped: 7544 });
+  });
+
+
+});
+
+test("throws error if empty request body", async function () {
+  const resp = await request(app).post("/shipments").send();
+  expect(resp.statusCode).toEqual(400);
+});
+
+test("throws error if invalid request body", async function () {
+  const resp = await request(app).post("/shipments").send({
+    productId: "ds",
+    name: "okay",
+    addr: true,
+    zip: "2139019",
+  });
+
+  expect(resp.statusCode).toEqual(400);
+  expect(resp.body).toEqual({
+    error: {
+      message: [
+        "instance.productId is not of a type(s) integer",
+        "instance.addr is not of a type(s) string",
+      ],
+      status: 400,
+    },
   });
 });
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,15 +1,16 @@
 "use strict";
 
-const { shipProduct, SHIPIT_SHIP_URL, SHIPIT_API_KEY } = require("./shipItApi");
 const fetchMock = require("fetch-mock");
+const { shipProduct, SHIPIT_SHIP_URL, SHIPIT_API_KEY } = require("./shipItApi");
 
 test("shipProduct", async function () {
   fetchMock.post(SHIPIT_SHIP_URL, {
-    shipped: {
+    receipt: {
       itemId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
       zip: "12345-6789",
+      // key: SHIPIT_API_KEY,
       shipId: 5272,
     },
   });
@@ -21,5 +22,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(5272);
 });
